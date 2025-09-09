1a2e23708e9f5b535ca2ee0fd56fb5e5df04b4fe
hubert <hubert.gorski@bowwe.com>
2025-09-09 14:38:08 +0200

    change png to webp

--- FoxTales/FoxTales.Infrastructure/Data/Seeders/AvatarsSeeder.cs
+++ FoxTales/FoxTales.Infrastructure/Data/Seeders/AvatarsSeeder.cs
@@ -28,35 +28,35 @@ public class AvatarsSeeder(FoxTalesDbContext context, ILogger<AvatarsSeeder> log
                 {
                     AvatarId = (int)AvatarName.Default,
                     Name = AvatarName.Default,
-                    Source = $"/src/assets/imgs/defaultAvatars/{(int)AvatarName.Default}.png",
+                    Source = $"/src/assets/imgs/defaultAvatars/{(int)AvatarName.Default}.webp",
                     IsPremium = false,
                 },
                 new()
                 {
                     AvatarId = (int)AvatarName.Crazy,
                     Name = AvatarName.Crazy,
-                    Source = $"/src/assets/imgs/defaultAvatars/{(int)AvatarName.Crazy}.png",
+                    Source = $"/src/assets/imgs/defaultAvatars/{(int)AvatarName.Crazy}.webp",
                     IsPremium = false,
                 },
                 new()
                 {
                     AvatarId = (int)AvatarName.Happy,
                     Name = AvatarName.Happy,
-                    Source = $"/src/assets/imgs/defaultAvatars/{(int)AvatarName.Happy}.png",
+                    Source = $"/src/assets/imgs/defaultAvatars/{(int)AvatarName.Happy}.webp",
                     IsPremium = true,
                 },
                 new()
                 {
                     AvatarId = (int)AvatarName.New,
                     Name = AvatarName.New,
-                    Source = $"/src/assets/imgs/defaultAvatars/{(int)AvatarName.New}.png",
+                    Source = $"/src/assets/imgs/defaultAvatars/{(int)AvatarName.New}.webp",
                     IsPremium = true,
                 },
                 new()
                 {
                     AvatarId = (int)AvatarName.Sad,
                     Name = AvatarName.Sad,
-                    Source = $"/src/assets/imgs/defaultAvatars/{(int)AvatarName.Sad}.png",
+                    Source = $"/src/assets/imgs/defaultAvatars/{(int)AvatarName.Sad}.webp",
                     IsPremium = true,
                 }
             };
