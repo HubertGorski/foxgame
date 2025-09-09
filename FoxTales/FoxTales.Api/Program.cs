002ee14c9945a206e86ee25bc517a68c167d565f
hubert <hubert.gorski@bowwe.com>
2025-09-09 20:17:34 +0200

    add preview

--- FoxTales/FoxTales.Api/Program.cs
+++ FoxTales/FoxTales.Api/Program.cs
@@ -28,6 +28,7 @@ builder.Services.AddCors(options =>
     {
         policy.WithOrigins(
                 "http://localhost:5173",
+                "http://localhost:4173",
                 "https://foxtales.cc",
                 "https://www.foxtales.cc"
             )
